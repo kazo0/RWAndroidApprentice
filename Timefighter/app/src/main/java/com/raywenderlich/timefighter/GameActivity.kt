fe2599b10ea1a650ab6b2abdda2934211b170833
Steve Bilogan <steve.bilogan@nventive.com>
2018-07-11 21:30:35 -0400

    begin resetGame logic

--- Timefighter/app/src/main/java/com/raywenderlich/timefighter/GameActivity.kt
+++ Timefighter/app/src/main/java/com/raywenderlich/timefighter/GameActivity.kt
@@ -11,19 +11,54 @@ class GameActivity : AppCompatActivity() {
     internal lateinit var gameScoreTextView: TextView
     internal lateinit var timeLeftTextView: TextView
     internal lateinit var tapMeButton: Button
+    internal var score = 0
+    internal var gameStarted = false
+
+    internal lateinit var countDownTimer: CountDownTimer
+    internal var initialCountDown: Long = 60000
+    internal var countDownInterval: Long = 1000
+    internal var timeLeft: Long = 60
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
-        // connect views to variables
+
+        gameScoreTextView = findViewById(R.id.game_score_text_view)
+        timeLeftTextView = findViewById(R.id.time_left_text_view)
+        tapMeButton = findViewById(R.id.tap_me_button)
+
+        tapMeButton.setOnClickListener { v -> incrementScore() }
     }
 
     private fun incrementScore() {
-        // increment score logic
+        score++
+
+        val newScore = getString(R.string.your_score, Integer.toString(score))
+        gameScoreTextView.text = newScore
     }
 
     private fun resetGame() {
-        // reset game logic
+        score = 0
+
+        val initialScore = getString(R.string.your_score, Integer.toString(score))
+        gameScoreTextView.text = initialScore
+
+        val initialTimeLeft = getString(R.string.time_left, "60")
+        timeLeftTextView.text = initialTimeLeft
+
+        countDownTimer = object : CountDownTimer(initialCountDown, countDownInterval) {
+            override fun onTick(millisUntilFinished: Long) {
+                timeLeft = millisUntilFinished / 1000
+
+                timeLeftTextView.text = getString(R.string.time_left, timeLeft.toString())
+            }
+
+            override fun onFinish() {
+
+            }
+        }
+
+        gameStarted = false
     }
 
     private fun startGame() {
