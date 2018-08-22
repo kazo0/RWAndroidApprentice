c8923f3be9d3c2ba0260dd81aa6e9a3948a886c2
Steve Bilogan <steve.bilogan@nventive.com>
2018-08-21 20:59:09 -0400

    Add initial RecyclerView

--- ListMaker/app/src/main/java/com/raywenderlich/listmaker/MainActivity.kt
+++ ListMaker/app/src/main/java/com/raywenderlich/listmaker/MainActivity.kt
@@ -3,12 +3,17 @@ package com.raywenderlich.listmaker
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutCompat
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.Menu
 import android.view.MenuItem
 
 import kotlinx.android.synthetic.main.activity_list.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var listsRecyclerView: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +24,10 @@ class MainActivity : AppCompatActivity() {
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
+
+        listsRecyclerView = findViewById<RecyclerView>(R.id.lists_recyclerview)
+        listsRecyclerView.layoutManager = LinearLayoutManager(this)
+        listsRecyclerView.adapter = ListSelectionRecyclerViewAdapter()
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
