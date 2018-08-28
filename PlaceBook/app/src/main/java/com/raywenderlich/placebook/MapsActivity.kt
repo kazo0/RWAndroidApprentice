299c7e88c0c25e5b68a98c9686b38c0209b326a5
Steve Bilogan <steve.bilogan@nventive.com>
2018-08-27 21:32:45 -0400

    Add calls to Google Places API

--- PlaceBook/app/src/main/java/com/raywenderlich/placebook/MapsActivity.kt
+++ PlaceBook/app/src/main/java/com/raywenderlich/placebook/MapsActivity.kt
@@ -1,11 +1,18 @@
 package com.raywenderlich.placebook
 
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.app.ActivityCompat
 import android.util.Log
+import android.widget.Toast
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.api.GoogleApiClient
 import com.google.android.gms.location.*
+import com.google.android.gms.location.places.Place
+import com.google.android.gms.location.places.PlacePhotoMetadata
+import com.google.android.gms.location.places.Places
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -13,12 +20,14 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.PointOfInterest
 import java.util.jar.Manifest
 
-class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleApiClient.OnConnectionFailedListener {
 
     private lateinit var map: GoogleMap
     private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    private lateinit var googleApiClient: GoogleApiClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,6 +43,11 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         map = googleMap
 
         getCurrentLocation()
+
+        setupGoogleClient()
+        map.setOnPoiClickListener {
+            displayPoi(it)
+        }
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
@@ -47,6 +61,11 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
             }
         }
     }
+
+    override fun onConnectionFailed(result: ConnectionResult) {
+        Log.e(TAG, "Google play connection failed: ${result.errorMessage}")
+    }
+
 
     private fun setupLocationClient() {
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
@@ -76,8 +95,67 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         }
     }
 
+    private fun setupGoogleClient() {
+        googleApiClient = GoogleApiClient.Builder(this)
+                .enableAutoManage(this, this)
+                .addApi(Places.GEO_DATA_API)
+                .build()
+    }
+
+    private fun displayPoi(poi: PointOfInterest) {
+        displayPoiGetPlaceStep(poi)
+    }
+
+    private fun displayPoiGetPlaceStep(poi: PointOfInterest) {
+        Places.GeoDataApi
+                .getPlaceById(googleApiClient, poi.placeId)
+                .setResultCallback { places ->
+                    if (places.status.isSuccess && places.count > 0) {
+                        val place = places[0].freeze()
+                        displayPoiGetPhotoMetaDataStep(place)
+                    } else {
+                        Log.e(TAG, "Error with getPlaceById: ${places.status.statusMessage}")
+                    }
+                    places.release()
+                }
+    }
+
+    private fun displayPoiGetPhotoMetaDataStep(place: Place) {
+        Places.GeoDataApi
+                .getPlacePhotos(googleApiClient, place.id)
+                .setResultCallback { result ->
+                    if (result.status.isSuccess) {
+                        val photoMetadataBuffer = result.photoMetadata
+
+                        if (photoMetadataBuffer.count > 0) {
+                            val photo = photoMetadataBuffer[0].freeze()
+                            displayPoiGetPhotoStep(place, photo)
+                        }
+                        photoMetadataBuffer.release()
+                    }
+                }
+    }
+
+    private fun displayPoiGetPhotoStep(place: Place, photo: PlacePhotoMetadata) {
+        photo.getScaledPhoto(
+                googleApiClient,
+                resources.getDimensionPixelSize(R.dimen.default_image_width),
+                resources.getDimensionPixelSize(R.dimen.default_image_height))
+                .setResultCallback { result ->
+                    if (result.status.isSuccess) {
+                        val image = result.bitmap
+                    } else {
+
+                    }
+                }
+    }
+
+    private fun displayPoiDisplayStep(place: Place, photo: Bitmap?) {
+
+    }
+
     companion object {
-        private const val REQUEST_LOCATION = 1;
+        private const val REQUEST_LOCATION = 1
         private const val TAG = "MapsActivity"
     }
 }
