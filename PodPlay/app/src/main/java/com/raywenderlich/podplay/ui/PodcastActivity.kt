54210ac965021795918e9ac955daeca6324030b6
Steve Bilogan <steve.bilogan@gmail.com>
2018-09-25 16:54:59 -0400

    Add SearchViewModel

--- PodPlay/app/src/main/java/com/raywenderlich/podplay/ui/PodcastActivity.kt
+++ PodPlay/app/src/main/java/com/raywenderlich/podplay/ui/PodcastActivity.kt
@@ -1,29 +1,68 @@
 package com.raywenderlich.podplay.ui
 
+import android.app.SearchManager
+import android.content.Context
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.SearchView
 import android.util.Log
+import android.view.Menu
 import com.raywenderlich.podplay.R
 import com.raywenderlich.podplay.repository.ItunesRepo
 import com.raywenderlich.podplay.service.ItunesService
-import com.raywenderlich.podplay.service.PodcastResponse
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import kotlinx.android.synthetic.main.activity_podcast.*
 
 class PodcastActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_podcast)
+        setupToolbar()
+    }
 
-        val TAG = javaClass.simpleName
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menu?.let { menu ->
+            menuInflater.inflate(R.menu.menu_search, menu)
+
+            val searchMenuItem = menu.findItem(R.id.search_item)
+            val searchView = searchMenuItem?.actionView as SearchView
+
+            val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+
+            searchView.setSearchableInfo(searchManager.getSearchableInfo(componentName))
+
+        }
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        setIntent(intent)
+        intent?.let { handleIntent(it) }
+    }
+
+    private fun handleIntent(intent: Intent) {
+        if (intent.action == Intent.ACTION_SEARCH) {
+            val query = intent.getStringExtra(SearchManager.QUERY)
+            performSearch(query)
+        }
+    }
+
+    private fun performSearch(term: String) {
         val itunesService = ItunesService.instance
         val itunesRepo = ItunesRepo(itunesService)
 
-        itunesRepo.searchByTerm("Android Developer") { podcasts ->
+        itunesRepo.searchByTerm(term) { podcasts ->
             Log.i(TAG, "Results = $podcasts")
-
         }
+    }
+
+    private fun setupToolbar() {
+        setSupportActionBar(toolbar)
+    }
+
+    companion object {
+        val TAG = javaClass.simpleName
     }
 }
---
+++ PodPlay/app/src/main/java/com/raywenderlich/podplay/viewmodel/SearchViewModel.kt
@@ -0,0 +1,40 @@
+package com.raywenderlich.podplay.viewmodel
+
+import android.app.Application
+import android.arch.lifecycle.AndroidViewModel
+import com.raywenderlich.podplay.repository.ItunesRepo
+import com.raywenderlich.podplay.service.PodcastResponse
+
+class SearchViewModel(application: Application): AndroidViewModel(application) {
+    var itunesRepo: ItunesRepo? = null
+
+    fun searchPodcasts(term: String, callback: (List<PodcastSummaryView>) -> Unit) {
+        itunesRepo?.searchByTerm(term) { results ->
+            if (results == null) {
+                callback(emptyList())
+            } else {
+                val searchViews = results.map { itunesPodcastToPodcastSummaryView(it) }
+                searchViews.let { callback(it) }
+            }
+        }
+    }
+
+    private fun itunesPodcastToPodcastSummaryView(
+            itunesPodcast: PodcastResponse.ItunesPodcast):
+            PodcastSummaryView {
+
+        return PodcastSummaryView(
+                itunesPodcast.collectionCensoredName,
+                itunesPodcast.releaseDate,
+                itunesPodcast.artworkUrl30,
+                itunesPodcast.feedUrl
+        )
+    }
+
+    data class PodcastSummaryView(
+            var name: String? = "",
+            var lastUpdated: String? = "",
+            var imageUrl: String? = "",
+            var feedUrl: String? = ""
+    )
+}
